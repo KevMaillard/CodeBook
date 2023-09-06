7c6511b5641001d41f8aa8f7eeeda32b57978e3d
KevMaillard <kemaillard@gmail.com>
2023-09-06 11:11:30 +0200

    suppr API / Add JSON

--- CodeBook_Back/src/controllers/borrowController.ts
+++ CodeBook_Back/src/controllers/borrowController.ts
@@ -1,65 +1,49 @@
-import axios from "axios";
-import { log } from "console";
-import { Request, Response } from "express";
-import { modelBook } from "../models/model";
+import * as fs from 'fs';
+import { Request, Response } from 'express';
+import { modelBook } from '../models/model';
 
 const borrowBook = async (req: Request, res: Response) => {
-    const resultAxios = await axios
-        .get("http://141.94.247.187:3000/api/v1/list")
-        .then((response) => {
-            return response.data;
-        });
+    // Lire le fichier JSON local
+    fs.readFile('users.json', 'utf8', async (err, data) => {
+        if (err) {
+            console.error('Erreur de lecture du fichier JSON :', err);
+            res.status(500).json({ message: 'Erreur interne du serveur' });
+            return;
+        }
 
-    let code_user = false;
+        try {
+            const users = JSON.parse(data);
+            console.log(users);
+            const { user_id } = req.body;
+            let code_user = false;
 
-    const data = req.body
+            users.forEach((user: { code: string }) => {
+                if (user.code === user_id) {
+                    code_user = true;
+                }
+            });
 
-    resultAxios.forEach((item: any) => {
-        if (item.code === data.user_id) {
-            code_user = true;
+            let borrow = await modelBook.findOne({ title: req.params.title });
+
+            switch (borrow) {
+                case null:
+                case undefined:
+                    res.status(404).json('Rien trouvé');
+                    break;
+                default:
+                    borrow.user_id = user_id;
+                    borrow.borrow_date = new Date();
+                    borrow.available = false;
+                    borrow.self_service_id = undefined;
+                    await borrow.save();
+                    res.status(200).json({ message: 'Livre emprunté', borrow });
+                    break;
+            }
+        } catch (parseError) {
+            console.error('Erreur d\'analyse JSON :', parseError);
+            res.status(500).json({ message: 'Erreur interne du serveur' });
         }
     });
-
-    let borrow = await modelBook.findOne({title: req.params.title })
-    
- 
-    switch(borrow) {
-        case null: 
-            res.status(404).json("Rien trouver")
-        break;
-        case undefined: 
-            res.status(404).json("Rien trouver")
-        break;
-        // 
-        default: 
-            borrow.user_id = data.user_id
-            borrow.borrow_date = new Date()
-            borrow.available = false
-            borrow.self_service_id = undefined
-            borrow.save()
-            res.status(200).json({message: 'Book borrowed', borrow})
-        break;
-    }
-
-
-
-
-    // if (code_user) {
-    //     req.body.user_id = {user_id}
-    //     req.body.user_id == null
-    //         ? req.body.borrow_date = new Date()
-    //         : req.body.borrow_date = null
-    //     req.body.user_id == null
-    //         ? req.body.available = true
-    //         : req.body.available = false;
-    //     const result = await modelBook.findOneAndUpdate(
-    //         { title: req.params.title },
-    //         req.body.user_id
-    //     );
-    //     res.status(200).json({ message: "book borrow", result });
-    // } else {
-    //     res.status(401).json({ message: "Wrong code user" });
-    // }
 };
 
 export default {
--- CodeBook_Back/src/controllers/loginController.ts
+++ CodeBook_Back/src/controllers/loginController.ts
@@ -1,27 +1,38 @@
+import { Request, Response } from 'express';
+import * as fs from 'fs';
 
-import axios from "axios"
-import { Request, Response } from "express"
+const login = async (req: Request, res: Response) => {
+    // Lire le fichier JSON local
+    fs.readFile('users.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error('Erreur de lecture du fichier JSON :', err);
+            res.status(500).json({ message: 'Erreur interne du serveur' });
+            return;
+        }
 
-const login = async (req:Request, res: Response) => {
-    const result = await axios.get('http://141.94.247.187:3000/api/v1/list').then((response) => {
-        return response.data
-    })
-    const {code, name}  = req.body
-    let isConnnect = false
+        try {
+            const users = JSON.parse(data);
 
-    result.forEach((item:any) => {
-        if(item.code === code && item.name === name){
-            isConnnect = true
+            const { code, name } = req.body;
+            let isConnect = false;
+
+            users.forEach((user: { code: string; name: string }) => {
+                if (user.code === code && user.name === name) {
+                    isConnect = true;
+                }
+            });
+
+            if (isConnect) {
+                res.status(200).json({ message: 'ConnectÃ©' });
+            } else {
+                res.status(401).json({ message: 'Identifiants incorrects' });
+            }
+        } catch (parseError) {
+            console.error('Erreur d\'analyse JSON :', parseError);
+            res.status(500).json({ message: 'Erreur interne du serveur' });
         }
     });
-
-    if(isConnnect){
-        res.status(200).json({message : "Logged In"})
-    }else{
-        res.status(401).json({message : "Wrong Credentials"})
-    }
-
-}
+};
 
 export default {
     login
